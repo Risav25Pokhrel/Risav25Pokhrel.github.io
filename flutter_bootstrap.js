86c118095d01ab5a2cffe41fe54483e3ed59612f
Rissav25Pokhrel <risav.pok@gmail.com>
2025-03-14 17:45:12 +0545

    hovering Icon

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "110523725"
+    serviceWorkerVersion: "4121160277"
   }
 });
